5410acbe189a29df770ff94ef9b42efd24c0251e
Mariane Previde <mprevide@cpqd.com.br>
2019-09-30 13:36:10 -0300

    Update deploy-gh-pages.sh

--- travis/deploy-gh-pages.sh
+++ travis/deploy-gh-pages.sh
@@ -15,7 +15,7 @@ else
   export VERSION="${TRAVIS_BRANCH}"
 fi
 
-docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/api.apib -o - > ./apiary_${VERSION}.html
+docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/apiary.apib -o - > ./apiary_${VERSION}.html
 
 git add apiary_${VERSION}.html
 git commit -m 'Updating gh-pages' 
