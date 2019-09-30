aae90505534c9cf854c5e0a5596764e207300b7b
Mariane Previde <mprevide@cpqd.com.br>
2019-09-30 13:41:20 -0300

    Update deploy-gh-pages.sh

--- travis/deploy-gh-pages.sh
+++ travis/deploy-gh-pages.sh
@@ -1,26 +1,28 @@
-git remote add -f origin-gh-pages https://github.com/${TRAVIS_REPO_SLUG}
-git fetch origin-gh-pages
-git checkout . 
-git checkout -b temp-pages origin-gh-pages/gh-pages -f
-git checkout origin-gh-pages/${TRAVIS_BRANCH} -- ./docs
-rm -fr ./_static
-mv docs/* . -f
-git rm -r docs
+if [ $TRAVIS_PULL_REQUEST == false ] ; then
+    git remote add -f origin-gh-pages https://github.com/${TRAVIS_REPO_SLUG}
+    git fetch origin-gh-pages
+    git checkout . 
+    git checkout -b temp-pages origin-gh-pages/gh-pages -f
+    git checkout origin-gh-pages/${TRAVIS_BRANCH} -- ./docs
+    rm -fr ./_static
+    mv docs/* . -f
+    git rm -r docs
 
 
-if [ "${TRAVIS_BRANCH}" == "master" ]
-then
-  export VERSION="latest"
-else
-  export VERSION="${TRAVIS_BRANCH}"
+    if [ "${TRAVIS_BRANCH}" == "master" ]
+    then
+    export VERSION="latest"
+    else
+    export VERSION="${TRAVIS_BRANCH}"
+    fi
+
+    docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/api.apib -o - > ./apiary_${VERSION}.html
+
+    git add apiary_${VERSION}.html
+    git commit -m 'Updating gh-pages' 
+    git push --force http://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG} temp-pages:gh-pages
+
+    git checkout . 
+    git clean -fd
+    git checkout ${TRAVIS_BRANCH} origin-gh-pages/${TRAVIS_BRANCH} 
 fi
-
-docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/api.apib -o - > ./apiary_${VERSION}.html
-
-git add apiary_${VERSION}.html
-git commit -m 'Updating gh-pages' 
-git push --force http://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG} temp-pages:gh-pages
-
-git checkout . 
-git clean -fd
-git checkout ${TRAVIS_BRANCH} origin-gh-pages/${TRAVIS_BRANCH} 
