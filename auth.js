480c137d091cc21be431efd82ab1ea80422aed1d
Gilmaicor Leandro <gilmaicor357@gmail.com>
2021-04-14 11:55:31 -0300

    Revert "Changed dojot-modules library to SDK"
    This reverts commit 887f235f50e06015edc8e28e06d8d92da88a1d8b.

--- auth.js
+++ auth.js
@@ -1,29 +1,25 @@
 "use strict";
-const config = require("./config");
-const { Logger } = require("@dojot/microservice-sdk");
+const logger = require("@dojot/dojot-module-logger").logger
 
 function b64decode(data) {
-  if (typeof Buffer.from === 'function') {
+  if (typeof Buffer.from === "function") {
     return Buffer.from(data, 'base64').toString();
   } else {
-    return new Buffer(data, 'base64').toString();
+    return (new Buffer(data, 'base64')).toString();
   }
 }
 
 class UnauthorizedError {
-  constructor() {
-    this.message = 'Authentication (JWT) required for API';
+  constructor(){
+    this.message = "Authentication (JWT) required for API";
   }
 }
 
 class InvalidTokenError {
-  constructor() {
-    this.message = 'Invalid authentication token given';
+  constructor(){
+    this.message = "Invalid authentication token given";
   }
 }
-
-// Logger configuration
-const logger = new Logger('auth');
 
 function authParse(req, res, next) {
   const rawToken = req.get('authorization');
@@ -46,22 +42,18 @@ function authParse(req, res, next) {
 }
 
 function authEnforce(req, res, next) {
-  if (req.path.match(/(\.png|svg$)|(keymap\.json$)/)) {
+  if (req.path.match(/(\.png|svg$)|(keymap\.json$)/)){
     logger.debug(`will ignore ${req.path}`);
     return next();
   }
 
-  if (req.user === undefined || req.user.trim() === '') {
+  if (req.user === undefined || req.user.trim() === "" ) {
     // valid token must be supplied
-    logger.error(
-      `Got invalid request: user is not defined in token: ${req.get(
-        'authorization'
-      )}`
-    );
+    logger.error(`Got invalid request: user is not defined in token: ${req.get('authorization')}`);
     return res.status(401).send(new UnauthorizedError());
   }
 
-  if (req.service === undefined || req.service.trim() === '') {
+  if (req.service === undefined || req.service.trim() === "" ) {
     // valid token must be supplied
     return res.status(401).send(new UnauthorizedError());
   }
@@ -71,5 +63,5 @@ function authEnforce(req, res, next) {
 
 module.exports = {
   authParse: authParse,
-  authEnforce: authEnforce,
+  authEnforce: authEnforce
 };
--- server.js
+++ server.js
@@ -1,40 +1,23 @@
 "use strict";
 
-const cron = require("./cron");
-const api = require("./api");
-const config = require("./config");
-const healthcheck = require("./healthcheck");
-const { Logger } = require("@dojot/microservice-sdk");
+const cron = require('./cron');
+const api = require('./api');
+const config = require('./config')
+const healthcheck = require('./healthcheck');
+const logger = require("@dojot/dojot-module-logger").logger;
 
-// Logger configuration
-Logger.setVerbose(config.logger.verbose);
-Logger.setTransport('console', { level: config.logger['console.level'] });
-if (config.logger.file.enable) {
-  Logger.setTransport('file', {
-    level: config.logger.file.level,
-    filename: config.logger.file.filename,
-  });
-}
-const logger = new Logger('server');
+logger.setLevel(config.logger.level);
 
 process.on('unhandledRejection', (reason) => {
-  logger.error(
-    `Unhandled Rejection at: ${reason.stack || reason}. Bailing out!!`
-  );
-  process.kill(process.pid, 'SIGTERM');
+    logger.error(`Unhandled Rejection at: ${reason.stack || reason}. Bailing out!!`);
+    process.kill(process.pid, "SIGTERM");
 });
 
-let cronManager = new cron.CronManager();
-
-cronManager
-  .init()
-  .then(() => {
+var cronManager = new cron.CronManager();
+cronManager.init().then(() => {
     healthcheck.init(cronManager);
     api.init(cronManager, healthcheck.get());
-  })
-  .catch((error) => {
-    logger.error(
-      `Cron service initialization failed (${error}). Bailing out!!`
-    );
-    process.kill(process.pid, 'SIGTERM');
-  });
+}).catch(error => {
+    logger.error(`Cron service initialization failed (${error}). Bailing out!!`);
+    process.kill(process.pid, "SIGTERM");
+});
